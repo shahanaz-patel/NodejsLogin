5361f2c55b8ec33f883488abf9c3612e1570ad84
Shahnaz <shanazkukz@gmail.com>
2018-06-23 16:32:38 +0530

    beginning

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const mongoose = require('mongoose');
+const app = express();
+
+//Connect to mongoose
+mongoose.connect('mongodb://localhost/nodelogin-dev', {
+    useMongoClient: true
+})
+.then(() => console.log("MongoDB Connected..."))
+.catch(err => console.log(err))
+
+//Handlebars Middleware
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
+app.get('/',(req,res) => {
+    res.render('index');
+});
+
+app.get('/about',(req,res) => {
+    res.render('about');
+});
+
+const port = 5000;
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
